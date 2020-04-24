a4e8092a40060e6256bbbd750fa488871363f41e
johnathan olivas <jj.galvan13@yahoo.com>
2020-04-23 18:46:49 -0600

    altered serverjs

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -12,11 +18,12 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
